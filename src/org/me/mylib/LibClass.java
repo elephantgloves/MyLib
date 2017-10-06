1885487659c76079ddd36bf4e17ffe4554cbd135
elephantgloves <tachibana.yki@gmail.com>
2017-10-06 22:25:15 +0900

    more stuff

--- src/org/me/mylib/LibClass.java
+++ src/org/me/mylib/LibClass.java
@@ -4,7 +4,7 @@ package org.me.mylib;
 
 public class LibClass {
     public static String acrostic(String[] args){
-        //comment for A
+        //comment for A and otherwise
         StringBuffer b = new StringBuffer();
         for (int i = 0; i < args.length; i++) {
             if (args[i].length() > i){
